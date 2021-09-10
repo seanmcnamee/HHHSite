93d5939831485a2511606080e0bb1a38494df37d
Sean McNamee <seanmcnamee45@gmail.com>
2021-09-09 21:08:11 -0400

    updated for para contracts 7/1/21

--- src/validation.js
+++ src/validation.js
@@ -13,3 +13,29 @@ function validateRadioButton(num, strErrHelp) {
     alert(`Please select the ${strErrHelp}`);
     return false;
 }
+
+function getRadioButtonValue(radioButtonElement) {
+    var selectedRadioButton = -1;
+    for (const element of radioButtonElement.children) {
+        if (element.checked) {
+            selectedRadioButton = element.value;
+            break;
+        }
+    }
+    return selectedRadioButton;
+}
+
+function setVisible(element, isVisible) {
+    element.classList.add((isVisible)? "shown": "hidden");
+    element.classList.remove((isVisible)? "hidden": "shown");
+}
+
+function setDisplaying(element, isVisible) {
+    if (isVisible) {
+        element.classList.remove("not-displaying");
+    } else {
+        element.classList.add("not-displaying");
+    }
+    
+    
+}
